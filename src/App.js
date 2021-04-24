56c4a0c10e0a73ced55a53025a5e803d3caeae04
docen67 <docen67@hotmail.com>
2021-04-24 02:33:31 -0500

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import {
   ChakraProvider,
   Box,
   Text,
-  Link,
   VStack,
   Code,
   Grid,
   theme,
+  Button
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
 
 function App() {
   return (
@@ -19,19 +18,12 @@ function App() {
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
             <Text>
               Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
             </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+            <Button colorScheme="teal" size="lg">
+              Button
+            </Button>
           </VStack>
         </Grid>
       </Box>
